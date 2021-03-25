86a4d6876e78a0a3542eb6d07e4c56a6dd838bed
Alex Kravecas <akravecas@yahoo.com>
2021-03-25 17:28:27 -0400

    Completed Assignment

--- index.js
+++ index.js
@@ -1,1 +1,45 @@
 // Write your solution here!
+const cats =["Milo", "Otis", "Garfield"];
+
+console.log(cats)
+
+function destructivelyAppendCat() {
+    cats.push("Ralph");
+}
+
+function destructivelyPrependCat() {
+    cats.unshift("Bob");
+}
+
+function destructivelyRemoveLastCat() {
+    cats.pop();
+}
+
+function destructivelyRemoveFirstCat() {
+    cats.shift();
+}
+
+function appendCat() {
+    const copyCats = [...cats];
+    copyCats.push("Broom");
+    return copyCats
+}
+
+function prependCat() {
+    const copyCats = [...cats];
+    copyCats.unshift("Arnold");
+    return copyCats
+}
+
+
+function removeLastCat() {
+    const copyCats = [...cats];
+    copyCats.pop();
+    return copyCats
+}
+
+function removeFirstCat() {
+    const copyCats = [...cats];
+    copyCats.shift();
+    return copyCats
+}
